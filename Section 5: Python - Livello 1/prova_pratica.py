2ae349759840881248a071b47009972b46a38960
thepiratedude <thepiratedude@proton.me>
2024-10-03 12:04:05 +0200

    First commit and First lesson final test

---
+++ Section 5: Python - Livello 1/prova_pratica.py
@@ -0,0 +1,5 @@
+print("""Benvenuti al programma \"Moltiplicatore\"
+Restituisce in output il risultato di una moltiplicazione tra due numeri\n""")
+x = float(input("Inserisci il primo valore => "))
+y = float(input("Inserisci il secondo valore => "))
+print("Il risultato della moltiplicazione è: ", (x * y))
